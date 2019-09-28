e2c8537ac27c9f2d49f6af813a6c6a5b46f00b29
udo-munk <udo.munk@freenet.de>
2019-09-28 18:26:57 +0200

    use logging

--- mosteksim/srcsim/config.c
+++ mosteksim/srcsim/config.c
@@ -8,7 +8,8 @@
  *
  * History:
  * 24-SEP-19 (Mike Douglas) created for Mostek AID-80F and SYS-80FT
- * 27-SEP-19 include newline \n as config.txt delimiter
+ * 27-SEP-19 (Mike Douglas) include newline \n as config.txt delimiter
+ * 28-SEP-19 (Udo Munk) use logging
  */
 
 #include <stdlib.h>
@@ -16,8 +17,11 @@
 #include <string.h>
 #include "sim.h"
 #include "simglb.h"
+#include "log.h"
 
 #define BUFSIZE 256	/* max line length of command buffer */
+
+static const char *TAG = "config";
 
 void config(void)
 {
@@ -25,7 +29,8 @@ void config(void)
 	char buf[BUFSIZE];
 	char *s, *t1, *t2;
 
-	printf("\n");
+	LOG(TAG, "");
+
 	if ((fp = fopen("conf/config.txt", "r")) != NULL) {
 		s = buf;
 		while (fgets(s, BUFSIZE, fp) != NULL) {
@@ -34,29 +39,32 @@ void config(void)
 			t1 = strtok(s, "= \t\r\n");
 			t2 = strtok(NULL, "= \t\r\n");
 
-			if (0 == strcmp(t1,"bootrom")) {
-				printf("\nBoot ROM: %s\n",t2);
-				strcpy(xfn,t2);
+			if (0 == strcmp(t1, "bootrom")) {
+				LOG(TAG, "\nBoot ROM: %s\n\n", t2);
+				strcpy(xfn, t2);
 				x_flag = 1;
 			}
-			else if (0 == strcmp(t1, "drive0"))
-				printf("Drive 0: %s\n",t2);
-
-			else if (0 == strcmp(t1, "drive1")) 
-				printf("Drive 1: %s\n",t2);
-
-			else if (0 == strcmp(t1, "drive2"))
-				printf("Drive 2: %s\n",t2);
-
-			else if (0 == strcmp(t1, "drive3"))
-				printf("Drive 3: %s\n",t2);
-
-			else 
-				printf("config.txt: unknown command: %s\n",s);
+			else if (0 == strcmp(t1, "drive0")) {
+				LOG(TAG, "\nDrive 0: %s\n", t2);
+			}
+			else if (0 == strcmp(t1, "drive1")) {
+				LOG(TAG, "Drive 1: %s\n", t2);
+			}
+			else if (0 == strcmp(t1, "drive2")) {
+				LOG(TAG, "Drive 2: %s\n", t2);
+			}
+			else if (0 == strcmp(t1, "drive3")) {
+				LOG(TAG, "Drive 3: %s\n", t2);
+			}
+			else {
+				LOGW(TAG, "unknown command: %s",s);
+			}
 		}
 		fclose(fp);
 	}
-	else
-		printf("Missing conf/config.txt file\n");
-	printf("\n");
+	else {
+		LOGW(TAG, "missing conf/config.txt file");
+	}
+
+	LOG(TAG, "");
 }
