524ea3ace48a529e61f9e1d21fc8cf3acdecd38a
udo-munk <udo.munk@freenet.de>
2019-10-17 17:08:18 +0200

    fix typo

--- mosteksim/srcsim/memory.c
+++ mosteksim/srcsim/memory.c
@@ -6,7 +6,7 @@
  * This module implements memory management for mosteksim
  *
  * History:
- * 15-SEP-19 (Mike Douglas) Created from memory.h in the z80sim
+ * 15-SEP-19 (Mike Douglas) Created from memory.c in the z80sim
  * 		directory. Emulate memory of the Mostek AID-80F and SYS-80FT
  *		computers by treating 0xe000-0xefff as ROM.
  */
