b5e67c4733f16b3b23095f581b0b8aad81ced652
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-04-19 04:29:31 +0200

    cpmrecv: exit() is unsafe in signal handlers. found by gcc static analyzer
    use sigaction() to guarantee non-restartable read().
    signal handler now just sets a global flag.

--- cpmsim/srctools/cpmrecv.c
+++ cpmsim/srctools/cpmrecv.c
@@ -20,11 +20,13 @@
 
 #define UNUSED(x) (void)(x)
 
-int fdin, fdout;
+int signal_catched;
 
 int main(int argc, char *argv[])
 {
 	char c;
+	int fdin, fdout;
+	static struct sigaction newact;
 	void int_handler(int);
 
 	fdout = 0;
@@ -38,25 +40,24 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGHUP, int_handler);
+	newact.sa_handler = SIG_IGN;
+	sigemptyset(&newact.sa_mask);
+	newact.sa_flags = 0;
+	sigaction(SIGINT, &newact, NULL);
+	sigaction(SIGQUIT, &newact, NULL);
+	newact.sa_handler = int_handler;
+	sigaction(SIGHUP, &newact, NULL);
 
-	for (;;) {
-		if (read(fdin, &c, 1) == 1) {
-			if (c != '\r') {
-				if (fdout == 0) {
-				  if ((fdout = creat(argv[1], 0644)) == -1) {
+	while (!signal_catched && read(fdin, &c, 1) == 1) {
+		if (c != '\r') {
+			if (fdout == 0) {
+				if ((fdout = creat(argv[1], 0644)) == -1) {
 					perror(argv[1]);
 					close(fdin);
 					exit(EXIT_FAILURE);
-				  }
 				}
-				write(fdout, &c, 1);
 			}
-		} else {
-			close(fdout);
-			return(EXIT_SUCCESS);
+			write(fdout, &c, 1);
 		}
 	}
 
@@ -70,7 +71,5 @@ void int_handler(int sig)
 {
 	UNUSED(sig);
 
-	close(fdin);
-	close(fdout);
-	exit(EXIT_SUCCESS);
+	signal_catched = 1;
 }
