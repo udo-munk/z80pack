20a85fff326275987b9a28e0b0089ed5a23cba18
Udo Munk <udo.munk@freenet.de>
2024-04-23 20:04:47 +0200

    work

--- picosim/picosim.c
+++ picosim/picosim.c
@@ -1,3 +1,14 @@
+/*
+ * Z80SIM  -  a Z80-CPU simulator
+ *
+ * Copyright (C) 2024 by Udo Munk
+ *
+ * This is the main program for a Raspberry Pico (W) board
+ */
+
+#include "pico/stdlib.h"
+
 int main(int argc, char *argv[])
 {
+	stdio_init_all();	/* initialize Pico stdio */
 }
