36210337bcd1fd358611ca4f62527562bc9d54c8
Udo Munk <udo.munk@freenet.de>
2024-04-24 12:19:15 +0200

    give a bit more life to the little guy

---
+++ picosim/config.c
@@ -0,0 +1,15 @@
+/*
+ * Z80SIM  -  a Z80-CPU simulator
+ *
+ * Copyright (C) 2024 by Udo Munk
+ *
+ * This module reads the system configuration file and sets
+ * global variables, so that the system can be configured.
+ *
+ * History:
+ * 20-APR-24 dummy, no configuration implemented yet
+ */
+
+void config(void)
+{
+}
--- picosim/picosim.c
+++ picosim/picosim.c
@@ -3,12 +3,23 @@
  *
  * Copyright (C) 2024 by Udo Munk
  *
- * This is the main program for a Raspberry Pico (W) board
+ * This is the main program for a Raspberry Pico (W) board,
+ * substitutes z80core/sim0.c
  */
 
 #include "pico/stdlib.h"
 
+extern void config(void);
+extern void init_memory(void);
+extern void init_cpu(void);
+
 int main(int argc, char *argv[])
 {
 	stdio_init_all();	/* initialize Pico stdio */
+
+	config();		/* read system configuration */
+	init_cpu();		/* initialise CPU */
+	init_memory();	/* initialise memory configuration */
+
+	// now we might be able to start the Z80 CPU here
 }
