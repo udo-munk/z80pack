026d465f53cda2be818d8606979bfd0d11f5b8ec
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-04-25 09:41:29 +0200

    add missing sleep_ms() prototype

--- picosim/sim.h
+++ picosim/sim.h
@@ -12,6 +12,7 @@
 /*#define CORE_LOG*/	/* don't use LOG() logging in core simulator */
 #define EXCLUDE_I8080	/* don't include 8080 emulation support */
 
+extern void sleep_ms(unsigned);
 #define SLEEP_MS(t)	sleep_ms(t)
 
 #include "simcore.h"
