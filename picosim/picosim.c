016f98c012444308bc980320d31b5fa797c4a85b
Udo Munk <udo.munk@freenet.de>
2024-04-25 16:04:23 +0200

    enable builtin LED when emulation stopps, done for Pico W

--- picosim/picosim.c
+++ picosim/picosim.c
@@ -11,6 +11,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "tusb.h"
+#ifdef PICO_W
+#include "pico/cyw43_arch.h"
+#endif
 
 /* Project includes */
 #include "sim.h"
@@ -27,6 +30,15 @@ int main(void)
 	stdio_init_all();	/* initialize Pico stdio */
 	while (!tud_cdc_connected())
 		sleep_ms(100);	/* wait until USB connected */
+#ifdef PICO_W			/* initialize Pico W cyw43 hardware */
+	if (cyw43_arch_init())
+	{
+		printf("Wi-Fi init failed\n");
+		return -1;
+	}
+#else
+				/* initialize Pico hardware */
+#endif
 
 	printf("Z80pack release %s, %s\n\n", RELEASE, COPYR);
 
@@ -35,6 +47,12 @@ int main(void)
 	init_memory();		/* initialise memory configuration */
 
 	run_cpu();		/* run the CPU with whatever is in memory */
+
+	/* switch builtin LED on */
+#ifdef PICO_W
+	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+#else
+#endif
 
 	putchar('\n');
 	report_cpu_error();	/* check for CPU emulation errors and report */
--- picosim/sim.h
+++ picosim/sim.h
@@ -6,6 +6,8 @@
  * This is the configuration for a Raspberry Pico (W) board
  */
 
+#define PICO_W		/* board we use, comment for Pico */
+
 #define DEF_CPU Z80     /* default CPU (Z80 or I8080) */
 #define CPU_SPEED 0     /* default CPU speed 0=unlimited */
 #define Z80_UNDOC       /* compile undocumented Z80 instructions */
