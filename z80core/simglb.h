8682e80a81ea75b505a035277ec872d35e45267b
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-05-01 10:28:55 +0200

    tidy-up simglb.[hc]
    simglb.h: order variables as in simglb.c, remove unused break_flag.
    simglb.c: parity looks more natural with 4 per line

--- z80core/simglb.h
+++ z80core/simglb.h
@@ -14,8 +14,13 @@ typedef unsigned long long Tstates_t;	/* uint64 for counting T-states */
 typedef enum { BUS_DMA_NONE, BUS_DMA_BYTE,
 	       BUS_DMA_BURST, BUS_DMA_CONTINUOUS } BusDMA_t;
 
-void start_bus_request(BusDMA_t mode, Tstates_t (*bus_master)(BYTE bus_ack));
-void end_bus_request(void);
+extern void start_bus_request(BusDMA_t, Tstates_t (*)(BYTE));
+extern void end_bus_request(void);
+
+extern void (*ice_before_go)(void);
+extern void (*ice_after_go)(void);
+extern void (*ice_cust_cmd)(char *, WORD *);
+extern void (*ice_cust_help)(void);
 
 extern int	cpu;
 
@@ -30,33 +35,25 @@ extern WORD	PC, SP;
 extern BYTE	IFF;
 extern Tstates_t T;
 extern unsigned long long cpu_start, cpu_stop;
-extern BYTE	io_port, io_data;
 
 #ifdef BUS_8080
 extern BYTE	cpu_bus;
 extern int	m1_step;
 #endif
 
-extern BYTE	cpu_state, bus_request;
-extern BusDMA_t bus_mode;
-extern Tstates_t (*dma_bus_master)(BYTE bus_ack);
-extern int	int_data;
+extern BYTE	io_port, io_data;
+extern int	busy_loop_cnt[];
 
-extern int	s_flag, l_flag, m_flag, x_flag, break_flag, i_flag, f_flag,
-		u_flag, r_flag, c_flag, M_flag, R_flag,
-		cpu_error, int_int, sb_next, int_protection;
-extern char	parity[];
+extern BYTE	cpu_state;
+extern int	cpu_error;
 #ifndef EXCLUDE_Z80
 extern int	int_nmi, int_mode;
 #endif
-
+extern int	int_int, int_data, int_protection;
+extern BYTE	bus_request;
+extern BusDMA_t bus_mode;
+extern Tstates_t (*dma_bus_master)(BYTE);
 extern int	tmax, cpu_needed;
-extern int	busy_loop_cnt[];
-
-extern char	xfn[];
-extern char	*diskdir, diskd[];
-extern char	confdir[], conffn[];
-extern char	rompath[];
 
 #ifdef HISIZE
 extern struct	history his[];
@@ -65,6 +62,7 @@ extern int	h_next, h_flag;
 
 #ifdef SBSIZE
 extern struct	softbreak soft[];
+extern int	sb_next;
 #endif
 
 extern long	t_states;
@@ -80,7 +78,12 @@ extern WORD 	address_switch;
 extern BYTE 	fp_led_output;
 #endif
 
-extern void (*ice_before_go)(void);
-extern void (*ice_after_go)(void);
-extern void (*ice_cust_cmd)(char *, WORD *);
-extern void (*ice_cust_help)(void);
+extern int	s_flag, l_flag, m_flag, x_flag, i_flag, f_flag,
+		u_flag, r_flag, c_flag, M_flag, R_flag;
+
+extern char	xfn[];
+extern char	*diskdir, diskd[];
+extern char	confdir[], conffn[];
+extern char	rompath[];
+
+extern char	parity[];
