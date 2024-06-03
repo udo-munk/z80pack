7aa17839741784020a0b9f3b9783153d196d5d73
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-06-03 15:43:49 +0200

    Revert "forgot to remove these"
    This reverts commit 3c488ab617d8f911741cc71e504fd9b8acba394c.

---
+++ iodevices/mds-isbc202.h
@@ -0,0 +1,16 @@
+/*
+ * Z80SIM  -  a Z80-CPU simulator
+ *
+ * Common I/O devices used by various simulated machines
+ *
+ * Copyright (C) 2024 by Thomas Eberhardt
+ *
+ * Simulation of an Intel Intellec iSBC 202 double density disk controller
+ *
+ * History:
+ */
+
+#ifndef MDS_ISBC202_INC
+#define MDS_ISBC202_INC
+
+#endif /* !MDS_ISBC202_INC */
---
+++ iodevices/mds-monitor.c
@@ -0,0 +1,12 @@
+/*
+ * Z80SIM  -  a Z80-CPU simulator
+ *
+ * Common I/O devices used by various simulated machines
+ *
+ * Copyright (C) 2024 by Thomas Eberhardt
+ *
+ * Simulation of an Intel Intellec MDS-800 monitor module.
+ * It includes the monitor ROM, TTY, CRT, PTR, PTP, and line printer.
+ *
+ * History:
+ */
