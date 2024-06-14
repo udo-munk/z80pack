80ffeccc9f1abd752e9ae48fc2ff80c15850bdb3
Udo Munk <udo.munk@freenet.de>
2024-06-14 08:36:31 +0200

    get rid of hw_config, this is easier to understand

--- picosim/picosim.c
+++ picosim/picosim.c
@@ -45,6 +45,28 @@
 #define DEL 0x7f /* delete */
 
 /* global variables for access to SPI MicroSD drive */
+
+/* Configuration of RP2040 hardware SPI object */
+static spi_t spi = {  
+	.hw_inst = spi0,  // RP2040 SPI component
+	.sck_gpio = 18,   // GPIO number (not Pico pin number)
+	.mosi_gpio = 19,
+	.miso_gpio = 16,
+	.baud_rate = 12 * 1000 * 1000   // Actual frequency: 10416666.
+};
+
+/* SPI Interface */
+static sd_spi_if_t spi_if = {
+	.spi = &spi,  // Pointer to the SPI driving this card
+	.ss_gpio = 17 // The SPI slave select GPIO for this SD card
+};
+
+/* Configuration of the SD Card socket object */
+static sd_card_t sd_card = {   
+	.type = SD_IF_SPI,
+	.spi_if_p = &spi_if  // Pointer to the SPI interface driving this card
+};
+
 FATFS fs;       /* FatFs on SDIO MicroSD */
 FIL sd_file;	/* at any time we have only one file open */
 FRESULT sd_res;	/* result code from FatFS */
@@ -58,6 +80,17 @@ extern void report_cpu_error(void), report_cpu_stats(void);
 
 uint64_t get_clock_us(void);
 void gpio_callback(uint, uint32_t);
+
+/* Callbacks used by the SD library */
+size_t sd_get_num() { return 1; }
+
+sd_card_t *sd_get_by_num(size_t num) {
+	if (num == 0) {
+		return &sd_card;
+	} else {
+		return NULL;
+	}
+}
 
 int main(void)
 {
