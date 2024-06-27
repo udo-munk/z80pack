4b8a68428e3ccb55c936d216fbdb8ce29c7760df
Udo Munk <udo.munk@freenet.de>
2024-06-27 20:34:34 +0200

    use consistent comments

--- picosim/picosim.c
+++ picosim/picosim.c
@@ -48,23 +48,23 @@
 
 /* Configuration of RP2040 hardware SPI object */
 static spi_t spi = {  
-	.hw_inst = spi0,  // RP2040 SPI component
-	.sck_gpio = 18,   // GPIO number (not Pico pin number)
+	.hw_inst = spi0,  /* RP2040 SPI component */
+	.sck_gpio = 18,   /* GPIO number (not Pico pin number) */
 	.mosi_gpio = 19,
 	.miso_gpio = 16,
-	.baud_rate = 12 * 1000 * 1000   // Actual frequency: 10416666.
+	.baud_rate = 12 * 1000 * 1000 /* Actual frequency: 10416666 */
 };
 
 /* SPI Interface */
 static sd_spi_if_t spi_if = {
-	.spi = &spi,  // Pointer to the SPI driving this card
-	.ss_gpio = 17 // The SPI slave select GPIO for this SD card
+	.spi = &spi,  /* Pointer to the SPI driving this card */
+	.ss_gpio = 17 /* The SPI slave select GPIO for this SD card */
 };
 
 /* Configuration of the SD Card socket object */
 static sd_card_t sd_card = {   
 	.type = SD_IF_SPI,
-	.spi_if_p = &spi_if  // Pointer to the SPI interface driving this card
+	.spi_if_p = &spi_if /* Pointer to the SPI interface driving this card */
 };
 
 FATFS fs;       /* FatFs on MicroSD */
