udo-munk/z80pack

commit da5f84f23880a7fd39afc254887c07d0ecf5d709

sim.h no longer includes simdefs.h.
Moved exatoi to new simutil.*, just doesn't fit anywhere else.

Moved cpmsim dskdef from sim.h to simio.h.

Removed trailing white space in David McNaughton files.

altairsim/srcsim/simcfg.c
@@ -31,26 +31,29 @@
  * 29-AUG-2021 new memory configuration sections
  */
 
-#include <stdint.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sim.h"
+#include "simdefs.h"
 #include "simglb.h"
-#include "log.h"
 #include "simmem.h"
+#include "simutil.h"
 #include "simcfg.h"
-#include "simcore.h"
+
 #include "altair-88-sio.h"
 #include "altair-88-2sio.h"
 #include "proctec-vdm.h"
 
-#define BUFSIZE 256	/* max line length of command buffer */
-
+#include "log.h"
 static const char *TAG = "config";
 
-int  fp_size = 800;		/* default frontpanel size */
-BYTE fp_port = 0;		/* default fp input port value */
+#define BUFSIZE 256	/* max line length of command buffer */
+
+int  fp_size = 800;	/* default frontpanel size */
+BYTE fp_port = 0;	/* default fp input port value */
 
 void config(void)
 {
@@ -64,14 +67,14 @@ void config(void)
 	int section = 0;
 
 	if (c_flag) {
-		strcpy(&fn[0], &conffn[0]);
+		strcpy(fn, conffn);
 	} else {
-		strcpy(&fn[0], &confdir[0]);
-		strcat(&fn[0], "/system.conf");
+		strcpy(fn, confdir);
+		strcat(fn, "/system.conf");
 	}
 
-	if ((fp = fopen(&fn[0], "r")) != NULL) {
-		s = &buf[0];
+	if ((fp = fopen(fn, "r")) != NULL) {
+		s = buf;
 		while (fgets(s, BUFSIZE, fp) != NULL) {
 			if ((*s == '\n') || (*s == '\r') || (*s == '#'))
 				continue;
@@ -222,9 +225,9 @@ void config(void)
 				fp_size = atoi(t2);
 #endif
 			} else if (!strcmp(t1, "vdm_bg")) {
-				strncpy(&bg_color[1], t2, 6);
+				strncpy(bg_color, t2, 6);
 			} else if (!strcmp(t1, "vdm_fg")) {
-				strncpy(&fg_color[1], t2, 6);
+				strncpy(fg_color, t2, 6);
 			} else if (!strcmp(t1, "vdm_scanlines")) {
 				if (*t2 != '0')
 					slf = 2;
@@ -290,7 +293,7 @@ void config(void)
 				LOGD(TAG, "Boot switch address at %04XH", _boot_switch[section]);
 			} else if (!strcmp(t1, "[MEMORY")) {
 				v1 = strtol(t2, &t3, 10);
-				if (t3[0] != ']' || v1 < 1 || v1 > MAXMEMSECT) {
+				if (*t3 != ']' || v1 < 1 || v1 > MAXMEMSECT) {
 					LOGW(TAG, "invalid MEMORY section number %d", v1);
 					continue;
 				}

cromemcosim/srcsim/simcfg.c
@@ -18,20 +18,22 @@
  * 30-AUG-2021 new memory configuration sections
  */
 
-#include <stdint.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sim.h"
+#include "simdefs.h"
 #include "simglb.h"
-#include "log.h"
 #include "simmem.h"
-#include "simcore.h"
+#include "simutil.h"
 #include "simcfg.h"
 
-#define BUFSIZE 256	/* max line length of command buffer */
-
+#include "log.h"
 static const char *TAG = "config";
+
+#define BUFSIZE 256	/* max line length of command buffer */
 
 int  fp_size = 800;		/* default frontpanel size */
 BYTE fp_port = 0x10;		/* default fp input port value */
@@ -49,13 +51,13 @@ void config(void)
 	int section = 0;
 
 	if (c_flag) {
-		strcpy(&fn[0], &conffn[0]);
+		strcpy(fn, &conffn[0]);
 	} else {
-		strcpy(&fn[0], &confdir[0]);
-		strcat(&fn[0], "/system.conf");
+		strcpy(fn, &confdir[0]);
+		strcat(fn, "/system.conf");
 	}
 
-	if ((fp = fopen(&fn[0], "r")) != NULL) {
+	if ((fp = fopen(fn, "r")) != NULL) {
 		s = &buf[0];
 		while (fgets(s, BUFSIZE, fp) != NULL) {
 			if ((*s == '\n') || (*s == '\r') || (*s == '#'))
@@ -149,7 +151,7 @@ void config(void)
 				LOGD(TAG, "Boot switch address at %04XH", boot_switch[section]);
 			} else if (!strcmp(t1, "[MEMORY")) {
 				v1 = strtol(t2, &t3, 10);
-				if (t3[0] != ']' || v1 < 1 || v1 > MAXMEMSECT) {
+				if (*t3 != ']' || v1 < 1 || v1 > MAXMEMSECT) {
 					LOGW(TAG, "invalid MEMORY section number %d", v1);
 					continue;
 				}

imsaisim/srcsim/simcfg.c
@@ -35,23 +35,26 @@
  * 29-AUG-2021 new memory configuration sections
  */
 
-#include <stdint.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sim.h"
+#include "simdefs.h"
 #include "simglb.h"
 #include "simmem.h"
+#include "simutil.h"
 #include "simcfg.h"
-#include "simcore.h"
-/* #define LOG_LOCAL_LEVEL LOG_DEBUG */
-#include "log.h"
+
 #include "imsai-sio2.h"
 #include "imsai-vio.h"
 
-#define BUFSIZE 256		/* max line length of command buffer */
-
+/* #define LOG_LOCAL_LEVEL LOG_DEBUG */
+#include "log.h"
 static const char *TAG = "config";
+
+#define BUFSIZE 256		/* max line length of command buffer */
 
 int  fp_size = 800;		/* default frontpanel size */
 BYTE fp_port = 0;		/* default fp input port value */
@@ -69,14 +72,14 @@ void config(void)
 	int section = 0;
 
 	if (c_flag) {
-		strcpy(&fn[0], &conffn[0]);
+		strcpy(fn, conffn);
 	} else {
-		strcpy(&fn[0], &confdir[0]);
-		strcat(&fn[0], "/system.conf");
+		strcpy(fn, confdir);
+		strcat(fn, "/system.conf");
 	}
 
-	if ((fp = fopen(&fn[0], "r")) != NULL) {
-		s = &buf[0];
+	if ((fp = fopen(fn, "r")) != NULL) {
+		s = buf;
 		while (fgets(s, BUFSIZE, fp) != NULL) {
 			if ((*s == '\n') || (*s == '\r') || (*s == '#'))
 				continue;
@@ -328,7 +331,7 @@ void config(void)
 				LOGD(TAG, "Power-on-jump address at %04XH", boot_switch[section]);
 			} else if (!strcmp(t1, "[MEMORY")) {
 				v1 = strtol(t2, &t3, 10);
-				if (t3[0] != ']' || v1 < 1 || v1 > MAXMEMSECT) {
+				if (*t3 != ']' || v1 < 1 || v1 > MAXMEMSECT) {
 					LOGW(TAG, "invalid MEMORY section number %d", v1);
 					continue;
 				}

intelmdssim/srcsim/simcfg.c
@@ -12,19 +12,23 @@
  * 07-JUN-2024 rewrite of the monitor ports and the timing thread
  */
 
-#include <stdint.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sim.h"
+#include "simdefs.h"
 #include "simglb.h"
 #include "simmem.h"
+#include "simcfg.h"
+
 #include "mds-monitor.h"
+
 #include "log.h"
+static const char *TAG = "config";
 
 #define BUFSIZE 256	/* max line length of command buffer */
-
-static const char *TAG = "config";
 
 int fp_size = 800;		/* default frontpanel size */
 
@@ -36,14 +40,14 @@ void config(void)
 	char fn[MAX_LFN - 1];
 
 	if (c_flag) {
-		strcpy(&fn[0], &conffn[0]);
+		strcpy(fn, conffn);
 	} else {
-		strcpy(&fn[0], &confdir[0]);
-		strcat(&fn[0], "/system.conf");
+		strcpy(fn, confdir);
+		strcat(fn, "/system.conf");
 	}
 
-	if ((fp = fopen(&fn[0], "r")) != NULL) {
-		s = &buf[0];
+	if ((fp = fopen(fn, "r")) != NULL) {
+		s = buf;
 		while (fgets(s, BUFSIZE, fp) != NULL) {
 			if ((*s == '\n') || (*s == '\r') || (*s == '#'))
 				continue;

mosteksim/srcsim/simcfg.c
@@ -14,17 +14,18 @@
  * 11-MAY-2024 (Thomas Eberhardt) add confdir/rompath option support
  */
 
-#include <stdint.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sim.h"
+#include "simdefs.h"
 #include "simglb.h"
+
 #include "log.h"
+static const char *TAG = "config";
 
 #define BUFSIZE 256	/* max line length of command buffer */
-
-static const char *TAG = "config";
 
 void config(void)
 {

picosim/simcfg.c
@@ -14,15 +14,17 @@
  * 03-JUN-2024 added directory list for code files and disk images
  */
 
-#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+#include "sim.h"
+#include "simdefs.h"
+#include "simglb.h"
+
 #include "f_util.h"
 #include "ff.h"
-#include "sim.h"
-#include "simglb.h"
 
 extern FIL sd_file;
 extern FRESULT sd_res;

commit efeeead5884518c930c01d2f0c2a7e490008dbb0

fix altair config file parsing broken in da5f84f238
also correct comment in z80asm

altairsim/srcsim/simcfg.c
@@ -225,9 +225,9 @@ void config(void)
 				fp_size = atoi(t2);
 #endif
 			} else if (!strcmp(t1, "vdm_bg")) {
-				strncpy(bg_color, t2, 6);
+				strncpy(&bg_color[1], t2, 6);
 			} else if (!strcmp(t1, "vdm_fg")) {
-				strncpy(fg_color, t2, 6);
+				strncpy(&fg_color[1], t2, 6);
 			} else if (!strcmp(t1, "vdm_scanlines")) {
 				if (*t2 != '0')
 					slf = 2;

z80asm/z80apfun.c
@@ -308,7 +308,8 @@ WORD op_dw(int pass, BYTE dummy1, BYTE dummy2, char *operand, BYTE *ops)
 }
 
 /*
- *	EJECT, PAGE, .PRINTX, PRINT, and TITLE
+ *	EJECT, PAGE, LIST, .LIST, NOLIST, .XLIST, .PRINTX, PRINT, TITLE,
+ *	.XALL, .LALL, .SALL, .SFCOND, and .LFCOND
  */
 WORD op_misc(int pass, BYTE op_code, BYTE dummy, char *operand, BYTE *ops)
 {